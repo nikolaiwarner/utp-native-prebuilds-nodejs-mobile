b7a0b39035e90a20a4b6b0d5aa7f6eb88a82f57d
Jim Pick <jim@backblend.com>
2018-06-24 20:59:15 -0700

    Add build scripts to regenerate prebuilds
    `make run build`

---
+++ build-ios-arm64.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+set -e
+
+topDir=`cd $(dirname $0); pwd`
+NODEJS_HEADERS_DIR=$topDir/node_modules/nodejs-mobile-react-native/ios/libnode
+NODEJS_MOBILE_GYP_BIN_FILE=$topDir/node_modules/nodejs-mobile-gyp/bin/node-gyp.js
+
+export GYP_DEFINES="OS=ios"
+export npm_config_nodedir="$NODEJS_HEADERS_DIR"
+export npm_config_node_gyp="$NODEJS_MOBILE_GYP_BIN_FILE"
+export npm_config_platform="ios"
+export npm_config_node_engine="chakracore"
+export npm_config_arch="arm64"
+export PREBUILD_ARCH=arm64
+export PREBUILD_PLATFORM=ios
+export PREBUILD_NODE_GYP="$NODEJS_MOBILE_GYP_BIN_FILE"
+
+if [ ! -d utp-native ]; then
+  git clone git@github.com:mafintosh/utp-native.git
+fi
+
+pushd utp-native
+
+UTP_NATIVE=1 npm install
+
+pushd node_modules
+rm -rf prebuildify
+git clone git@github.com:jimpick/prebuildify.git
+pushd prebuildify
+git checkout ios
+popd
+popd
+
+npx prebuildify --strip --platform=ios --arch=arm64 --target=node@8.0.0
+
+popd
+
+tar cf - -C utp-native prebuilds | tar xvf -
